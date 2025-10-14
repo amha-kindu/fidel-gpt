2469d22019bb2ccd1004c569e255eedc2abe6ac3
Amha Kindu <amhaznif@gmail.com>
2025-10-14 19:31:55 +0000

    add new tokenizers & update module initializations

--- model.py
+++ model.py
@@ -9,11 +9,10 @@ from cache import SlidingKVCache
 
 
 class Embedding(nn.Module):
-    def __init__(self, embed_dim: int, vocab_size: int, dropout: float = 0) -> None:
+    def __init__(self, config: ModelConfig) -> None:
         super().__init__()
-        self.embed_dim = embed_dim
-        self.dropout = nn.Dropout(dropout)
-        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.dropout = nn.Dropout(config.dropout)
+        self.embedding = nn.Embedding(config.vocab_size, config.embed_dim)
 
     # Input shape: x -> (N_BATCHES, SEQ_LEN)
     # Output shape: (N_BATCHES, SEQ_LEN, EMBED_DIM)
@@ -22,17 +21,17 @@ class Embedding(nn.Module):
 
 
 class PositionEncoder(nn.Module):
-    def __init__(self, seq_len: int, embed_dim: int, dropout: float):
+    def __init__(self, config: ModelConfig):
         super().__init__()
         
         # (SEQ_LEN, 1)
-        positions = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        positions = torch.arange(0, config.seq_len, dtype=torch.float).unsqueeze(1)
 
         # (EMBED_DIM//2,)
-        div_term = torch.exp(torch.arange(0, embed_dim, 2, dtype=torch.float) * -math.log(10000.0) / embed_dim)
+        div_term = torch.exp(torch.arange(0, config.embed_dim, 2, dtype=torch.float) * -math.log(10000.0) / config.embed_dim)
 
         # (SEQ_LEN, EMBED_DIM)
-        position_encodings: torch.Tensor = torch.zeros(seq_len, embed_dim)
+        position_encodings: torch.Tensor = torch.zeros(config.seq_len, config.embed_dim)
 
         # PE(positions, 2i) = sin(positions / (10000 ^ (2i/embed_dim)))
         # PE(positions, 2i) = sin(positions * exp(-2i * log(10000) / embed_dim))
@@ -54,19 +53,18 @@ class PositionEncoder(nn.Module):
 
 
 class MultiHeadAttentionBlock(nn.Module):
-    def __init__(self, embed_dim: int, dropout: float, heads: int):
-        assert embed_dim % heads == 0, "EMBED_DIM is not divisible by heads"
+    def __init__(self, config: ModelConfig):
+        assert config.embed_dim % config.heads == 0, "EMBED_DIM is not divisible by heads"
 
         super().__init__()
-        self.heads = heads
-        self.d_head: int = embed_dim // heads
+        self.heads = config.heads
+        self.d_head: int = config.embed_dim // config.heads
 
-        self.Wq: nn.Linear = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.Wk: nn.Linear = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.Wv: nn.Linear = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.Wo: nn.Linear = nn.Linear(embed_dim, embed_dim, bias=False)
-
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(config.dropout)
+        self.Wq: nn.Linear = nn.Linear(config.embed_dim, config.embed_dim, bias=False)
+        self.Wk: nn.Linear = nn.Linear(config.embed_dim, config.embed_dim, bias=False)
+        self.Wv: nn.Linear = nn.Linear(config.embed_dim, config.embed_dim, bias=False)
+        self.Wo: nn.Linear = nn.Linear(config.embed_dim, config.embed_dim, bias=False)
     
     # Input shape: x -> (N_BATCHES, SEQ_LEN, EMBED_DIM), mask -> (SEQ_LEN, SEQ_LEN)
     # Output shape: (N_BATCHES, SEQ_LEN, EMBED_DIM)
@@ -113,34 +111,36 @@ class MultiHeadAttentionBlock(nn.Module):
         output = output.transpose(1, 2)
 
         # (N_BATCHES, SEQ_LEN, HEADS, d_head) -> (N_BATCHES, SEQ_LEN, EMBED_DIM)
-        output = output.contiguous().view(value.shape[0], value.shape[2], -1)
+        output = output.contiguous().view(*x.shape[:-1], -1)
         
         return self.Wo(output), kv_cache
     
 
 class FeedForwardBlock(nn.Module):
-    def __init__(self, embed_dim: int, ff_dim: int, dropout: float):
+    def __init__(self, config: ModelConfig):
         super().__init__()
-        self.linear1 = nn.Linear(embed_dim, ff_dim)
-        self.linear2 = nn.Linear(ff_dim, embed_dim)
         self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(config.dropout)
+        self.linear1 = nn.Linear(config.embed_dim, config.ff_dim)
+        self.linear2 = nn.Linear(config.ff_dim, config.embed_dim)
 
     # Input shape: x -> (N_BATCHES, SEQ_LEN, EMBED_DIM)
     # Output shape: (N_BATCHES, SEQ_LEN, EMBED_DIM)
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear2(
-                self.gelu(self.linear1(x))
+            self.dropout(self.gelu(self.linear1(x)))
         )
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, embed_dim: int, ff_dim: int, dropout: float, heads: int):
+    def __init__(self, config: ModelConfig):
         super().__init__()
-        self.norm1 = nn.LayerNorm(embed_dim)
-        self.norm2 = nn.LayerNorm(embed_dim)
-        self.masked_multihead_attention = MultiHeadAttentionBlock(embed_dim, dropout, heads)
-        self.feed_forward = FeedForwardBlock(embed_dim, ff_dim, dropout)
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(config.dropout)
+        self.norm1 = nn.LayerNorm(config.embed_dim)
+        self.norm2 = nn.LayerNorm(config.embed_dim)
+        
+        self.feed_forward = FeedForwardBlock(config)
+        self.masked_multihead_attention = MultiHeadAttentionBlock(config)
     
     # Input shape: x -> (N_BATCHES, SEQ_LEN, EMBED_DIM), mask -> (SEQ_LEN, SEQ_LEN)
     # Output shape: (N_BATCHES, SEQ_LEN, EMBED_DIM)
@@ -151,16 +151,22 @@ class DecoderBlock(nn.Module):
         use_cache: bool = False,
         kv_cache: tuple[torch.Tensor, torch.Tensor] | None = None
     ) -> tuple[torch.Tensor, SlidingKVCache | None]:
-        x_update, kv_cache = self.masked_multihead_attention(x, mask, use_cache, kv_cache)
-        x = x + self.dropout(self.norm1(x_update))
-        x = x + self.dropout(self.norm2(self.feed_forward(x)))
+        # Post-Norm
+        # x_update, kv_cache = self.masked_multihead_attention(x, mask, use_cache, kv_cache)
+        # x = x + self.dropout(self.norm1(x_update))
+        # x = x + self.dropout(self.norm2(self.feed_forward(x)))
+        
+        # Pre-Norm
+        x_update, kv_cache = self.masked_multihead_attention(self.norm1(x), mask, use_cache, kv_cache)
+        x = x + self.dropout(x_update)
+        x = x + self.dropout(self.feed_forward(self.norm2(x)))
         return x, kv_cache
 
 
 class Projection(nn.Module):
-    def __init__(self, embed_dim: int, vocab_size: int):
+    def __init__(self, config: ModelConfig):
         super().__init__()
-        self.linear = nn.Linear(embed_dim, vocab_size)
+        self.linear = nn.Linear(config.embed_dim, config.vocab_size)
 
     # Input shape: x -> (N_BATCHES, SEQ_LEN, EMBED_DIM)
     # Output shape: (N_BATCHES, SEQ_LEN, VOCAB_SIZE)
@@ -172,11 +178,11 @@ class GPTmodel(nn.Module):
     def __init__(self, config: ModelConfig):
         super().__init__()
         self.config: ModelConfig = config
-
-        self.decoders = nn.ModuleList([DecoderBlock(config.embed_dim, config.ff_dim, config.dropout, config.heads) for _ in range(config.n_blocks)])
-        self.embedding = Embedding(config.embed_dim, config.vocab_size, config.dropout)
-        self.position_encoder = PositionEncoder(config.seq_len, config.embed_dim, config.dropout)
-        self.projection = Projection(config.embed_dim, config.vocab_size)
+        
+        self.embedding = Embedding(config)
+        self.projection = Projection(config)
+        self.position_encoder = PositionEncoder(config)
+        self.decoders = nn.ModuleList([DecoderBlock(config) for _ in range(config.n_blocks)])
 
     # Input shape: x -> (N_BATCHES, SEQ_LEN)
     # Output shape: (N_BATCHES, SEQ_LEN, EMBED_DIM)
